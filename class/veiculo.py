9ad078f44d6dcf9f06e8b7323f37a2fa7752a7ef
caioding <7caioc@gmail.com>
2024-10-16 09:45:40 -0400

    add class veiculo

---
+++ class/veiculo.py
@@ -0,0 +1,6 @@
+class Veiculo:
+    def __init__(self, marca, modelo, ano, valor_diario):
+        self.marca = marca
+        self.modelo = modelo
+        self.ano = ano
+        self.valor_diario = valor_diario
