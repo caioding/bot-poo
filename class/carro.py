5260bf9ca5a78a9e04a01f40132d1ac6250cd46b
Lucas Andrade <academicolucas>
2024-10-16 11:04:43 -0400

    Added Getters and Setters to class Carro

--- class/carro.py
+++ class/carro.py
@@ -1,4 +1,15 @@
 class Carro(Veiculo):
     def __init__(self, nome, marca, modelo, ano, valor_diario, tipo_combustivel):
         super().__init__(nome, marca, modelo, ano, valor_diario)
-        self.tipo_combustivel = tipo_combustivel
+        self.__tipo_combustivel = tipo_combustivel
+
+    @property
+    def tipo_combustivel(self):
+        return self.__tipo_combustivel
+    
+    @tipo_combustivel.setter
+    def tipo_combustivel(self):
+        self.__tipo_combustivel
+
+  
+    #### Aguardando método de calcular aluguel na class Veiculo
